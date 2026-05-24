7a0ba68c5c06989178b93d7f2e8e82419b5445bf
Daniel <mistercodermoose@gmail.com>
2026-05-24 07:33:41 -0400

    Fix vite requirements to host on github pages

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/seg3125-assignment1/',
   plugins: [
     react(),
     tailwindcss(),
